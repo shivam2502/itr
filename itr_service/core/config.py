2e92aa5109f1be6649f01687a7cd21c3c64ab578
shivam <shivamgupta250295@gmail.com>
2024-01-15 22:13:07 +0530

    Fix: Structural primary fixes

--- itr_service/core/config.py
+++ itr_service/core/config.py
@@ -1,15 +1,1 @@
-from pydantic_settings import BaseSettings
-import os
 
-class Settings(BaseSettings):
-    APP_NAME: str = os.getenv("APP_NAME", "iitr")
-    VERSION: str = os.getenv("VERSION", "1.0.0")
-    DESCRIPTION: str = os.getenv("DESCRIPTION", "An application for filing Income Tax Returns")
-
-    # Database Settings
-    # DATABASE_URL: str = "postgresql://username:password@localhost/db_name"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
--- itr_service/main.py
+++ itr_service/main.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI
-# from itr_service.api.endpoints import filing
-from core.config import settings
-# from itr_service.core.database import create_db_and_tables
+import uvicorn
+import os
+from dotenv import load_dotenv
 
-app = FastAPI(
-    title=settings.APP_NAME,
-    version=settings.VERSION,
-    description=settings.DESCRIPTION,
-)
+load_dotenv()
 
-# Include routes from different endpoints
-# app.include_router(authentication.router, prefix="/auth", tags=["Authentication"])
-# app.include_router(filing.router, prefix="/filing", tags=["Filing"])
-# app.include_router(user_data.router, prefix="/user", tags=["User Data"])
-
-# Initialize database and create tables (if needed)
-# create_db_and_tables()
+def creat_app() -> FastAPI:
+    app = FastAPI(
+        title=os.getenv("TITLE"),
+        version=os.getenv("VERSION"),
+        description=os.getenv("DESC")
+    )
+    return app
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    print(os.getenv("TITLE"))
+    app = creat_app()
+    uvicorn.run(app, host=os.getenv("HOST"), port=os.getenv("PORT"))
